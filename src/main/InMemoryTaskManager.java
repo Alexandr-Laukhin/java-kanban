4fd1d05ffa6a0b6ffbc78fbc127502eeaa26fe6a
Aleksander <las.l@mail.ru>
2025-01-26 20:49:15 +0300

    ФЗ 6, история бесконечная, последовательная, без копий, скорость повышена

--- src/main/InMemoryTaskManager.java
+++ src/main/InMemoryTaskManager.java
@@ -5,6 +5,7 @@ import classes.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 
 public class InMemoryTaskManager implements TaskManager {
@@ -75,11 +76,20 @@ public class InMemoryTaskManager implements TaskManager {
 
     @Override
     public void deleteTasks() {
+        for (Integer integer : tasks.keySet()) {
+            historyManager.remove(integer);
+        }
         tasks.clear();
     }
 
     @Override
     public void deleteEpics() {
+        for (Integer integer : epics.keySet()) {
+            historyManager.remove(integer);
+        }
+        for (Integer integer : subTasks.keySet()) {
+            historyManager.remove(integer);
+        }
         epics.clear();
         subTasks.clear();
     }
@@ -96,6 +106,7 @@ public class InMemoryTaskManager implements TaskManager {
     @Override
     public void deleteTaskByID(int taskID) {
         tasks.remove(taskID);
+        historyManager.remove(taskID);
     }
 
     @Override
@@ -103,9 +114,11 @@ public class InMemoryTaskManager implements TaskManager {
         ArrayList<Integer> epicSubtasksId = new ArrayList<>(epics.get(epicID).getSubTasksID());
 
         for (Integer integer : epicSubtasksId) {
+            historyManager.remove(integer);
             subTasks.remove(integer);
         }
         epics.remove(epicID);
+        historyManager.remove(epicID);
     }
 
     @Override
@@ -116,6 +129,7 @@ public class InMemoryTaskManager implements TaskManager {
 
         subTasks.remove(subTaskID);
         checkEpicStatus(epicId);
+        historyManager.remove(subTaskID);
     }
 
     @Override
@@ -123,8 +137,7 @@ public class InMemoryTaskManager implements TaskManager {
         Task task = tasks.get(taskID);
         historyManager.add(task);
         return task;
-    } // В таком ключе я, как обычно, не подумал. Спасибо, что обратил на это внимание, двойные обращения к хешмапам
-    // действительно менее аккуратно выглядят, и систему, как я понимаю, будут нагружать больше.
+    }
 
     @Override
     public Epic getEpicByID(int epicID) {
@@ -180,6 +193,4 @@ public class InMemoryTaskManager implements TaskManager {
             }
         }
     }
-
-
 }
